35f2392d5e8275705192241207a7f86b2c17eaad
João Darwin <joaodarwin.ip22@gmail.com>
2022-09-01 10:32:58 -0300

    Finalização da página Games

---
+++ Página Games/PC e Tablet/script.js
@@ -0,0 +1,11 @@
+let botaoMenu = document.getElementById("imgMenu").style
+
+function abrirOuFechar() {
+    if(document.getElementById("menuOculto").style.width == "30%") {
+        document.getElementById("menuOculto").style.width = "0%"
+        botaoMenu.rotate = "0deg"
+    } else{
+        document.getElementById("menuOculto").style.width = "30%"
+        botaoMenu.rotate = "90deg"
+    }
+}
